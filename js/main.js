ec1d5390be50eba7a22cbc96542c32a649c1b86c
darren <linfang92926@gmail.com>
2015-02-26 14:01:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,25 +17,25 @@
        	    	var getDom  = function(opts){
                     _this.wrap = _this.getId("wrap");
                     _this.img_small = _this.wrap.getElementsByTagName("img")[0];
-	       	   	    _this.img_clip = _this.wrap.getElementsByTagName("img")[1];
-	       	   	    _this.pop = _this.getId("pop");
-	       	   	    _this.preview = _this.getId("preview");
-	       	   	    _this.img_big = _this.preview.getElementsByTagName("img")[0];
-	       	   	    _this.widthMinus = opts.big_width / opts.small_width;
-	       	   	    _this.heightMinus = opts.big_height / opts.big_height;
-	       	   	    _this.method = opts.method;
+	       	    _this.img_clip = _this.wrap.getElementsByTagName("img")[1];
+       	   	    _this.pop = _this.getId("pop");
+       	   	    _this.preview = _this.getId("preview");
+       	   	    _this.img_big = _this.preview.getElementsByTagName("img")[0];
+       	   	    _this.widthMinus = opts.big_width / opts.small_width;
+       	   	    _this.heightMinus = opts.big_height / opts.big_height;
+       	   	    _this.method = opts.method;
 
        	    	};
 
        	    	var setStyle = function(opts){
-       	    		_this.img_small.style.filter = 'alpha(opacity:'+opts.opacity * 100+')';
-				    _this.img_small.style.opacity = opts.opacity ;
-				    _this.pop.style.width = opts.clip_width + "px";
-				    _this.pop.style.height = opts.clip_height + "px";
+                    _this.img_small.style.filter = 'alpha(opacity:'+opts.opacity * 100+')';
+		    _this.img_small.style.opacity = opts.opacity ;
+		    _this.pop.style.width = opts.clip_width + "px";
+	            _this.pop.style.height = opts.clip_height + "px";
        	    	}
        	    	_this.d.onselectstart = function(event){
-       	    		var e = EventUtil.getEvent(event);
-       	    		EventUtil.preventDefault(e);
+       	    	    var e = EventUtil.getEvent(event);
+       	    	    EventUtil.preventDefault(e);
        	    	}
 
                 getDom(opts);
@@ -43,7 +43,7 @@
        	    	this.renderPosition();
        	    	this.dragStart();
        	    	if(_this.method == "mouse"){
-       	    		_this.pop.style.display = 'none';
+       	           _this.pop.style.display = 'none';
                    _this.img_clip.style.display = 'none';
                    _this.preview.style.display = 'none';
                    _this.img_small.style.zIndex = '1000';
@@ -55,9 +55,9 @@
        	    	var _this = this;
        	    	var current_top = _this.pop.offsetTop;
        	    	var current_left = _this.pop.offsetLeft;
-				var current_right = _this.pop.offsetLeft + _this.pop.offsetWidth;
-				var current_bottom = _this.pop.offsetTop + _this.pop.offsetHeight;
-				_this.img_clip.style.clip = "rect("+current_top+"px,"+current_right+"px,"+current_bottom+"px,"+current_left+"px)";
+		var current_right = _this.pop.offsetLeft + _this.pop.offsetWidth;
+		var current_bottom = _this.pop.offsetTop + _this.pop.offsetHeight;
+		_this.img_clip.style.clip = "rect("+current_top+"px,"+current_right+"px,"+current_bottom+"px,"+current_left+"px)";
        	        _this.img_big.style.left = -(current_left * _this.widthMinus) + "px";
        	        _this.img_big.style.top = -(current_top * _this.heightMinus) + "px";
        	    },
@@ -69,43 +69,42 @@
        	    	var moveHandlder = function(ev){
 	                var e = EventUtil.getEvent(ev);
 	                if(_this.method == "move"){
-	                	var l = e.clientX-disX;
-					    var t = e.clientY-disY;
+	                    var l = e.clientX-disX;
+			    var t = e.clientY-disY;
 	                }else{
 	                    var l=e.clientX-_this.img_small.offsetLeft-_this.pop.offsetWidth/2;
-						var t=e.clientY-_this.img_small.offsetTop-_this.pop.offsetHeight/2;
+			    var t=e.clientY-_this.img_small.offsetTop-_this.pop.offsetHeight/2;
 	                }
 
-					if(l < 0){
-						l = 0;
-					}else if(l > _this.img_small.offsetWidth - _this.pop.offsetWidth) {
-						l = _this.img_small.offsetWidth - _this.pop.offsetWidth;
-					}
-
-					if(t < 0){
-						t = 0;
-					}else if(t > _this.img_small.offsetHeight - _this.pop.offsetHeight) {
-						t = _this.img_small.offsetHeight - _this.pop.offsetHeight;
-					}
-					_this.pop.style.left = l + 'px';
-					_this.pop.style.top = t + 'px';
-					_this.renderPosition();
-				};
+			if(l < 0){
+			    l = 0;
+			}else if(l > _this.img_small.offsetWidth - _this.pop.offsetWidth) {
+			    l = _this.img_small.offsetWidth - _this.pop.offsetWidth;
+			}
+			if(t < 0){
+			    t = 0;
+			}else if(t > _this.img_small.offsetHeight - _this.pop.offsetHeight) {
+			    t = _this.img_small.offsetHeight - _this.pop.offsetHeight;
+			}
+			   _this.pop.style.left = l + 'px';
+		           _this.pop.style.top = t + 'px';
+			   _this.renderPosition();
+			};
        	    	if(_this.method == "move"){
-					var downHandler = function(ev){
-						console.log(1);
-						var e = EventUtil.getEvent(ev);
-						disX = e.clientX - _this.pop.offsetLeft;
-						disY = e.clientY - _this.pop.offsetTop;
-						EventUtil.preventDefault(e);
-						EventUtil.stopPropagation(e);
-						EventUtil.addHandler(_this.d,"mousemove",moveHandlder);
-						EventUtil.addHandler(_this.d,"mouseup",upHandler);
-					};
-					var upHandler = function(){
-						EventUtil.removeHandler(_this.d,"mousemove",moveHandlder);
-						EventUtil.removeHandler(_this.d,"mouseup",upHandler);
-					};
+			var downHandler = function(ev){
+				console.log(1);
+				var e = EventUtil.getEvent(ev);
+				disX = e.clientX - _this.pop.offsetLeft;
+				disY = e.clientY - _this.pop.offsetTop;
+				EventUtil.preventDefault(e);
+				EventUtil.stopPropagation(e);
+				EventUtil.addHandler(_this.d,"mousemove",moveHandlder);
+				EventUtil.addHandler(_this.d,"mouseup",upHandler);
+			};
+			var upHandler = function(){
+				EventUtil.removeHandler(_this.d,"mousemove",moveHandlder);
+				EventUtil.removeHandler(_this.d,"mouseup",upHandler);
+			};
                 	EventUtil.addHandler(_this.pop,"mousedown",downHandler);
        	    	}else{
        	    		return {
